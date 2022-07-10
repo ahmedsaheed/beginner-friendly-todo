f2f89110dc7f077d27a0e2fb0bc45ee558d99f27
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-07-11 00:14:45 +0100

    added ts and dependecies

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+    arrowParens: 'avoid',
+    singleQuote: true,
+    backerSpace: true,
+    semi: false,
+    endOfLine: "lf",
+    tabWidth: 4,
+    trailingComma:"none",
+}
+
+module.exports = options;
